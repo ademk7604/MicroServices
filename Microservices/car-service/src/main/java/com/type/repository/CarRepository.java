5791408d69ce88bd07cd7577b895471cb9a26b31
adem kok <ademk760@gmail.com>
2023-05-09 14:21:20 +0200

    a

--- Microservices/car-service/src/main/java/com/type/repository/CarRepository.java
+++ Microservices/car-service/src/main/java/com/type/repository/CarRepository.java
@@ -1,9 +1,10 @@
 package com.type.repository;
 
 import com.type.domain.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarRepository extends CrudRepository<Car, Long> {
+public interface CarRepository extends JpaRepository<Car, Long> {
 }
--- Microservices/car-service/src/main/java/com/type/service/CarService.java
+++ Microservices/car-service/src/main/java/com/type/service/CarService.java
@@ -1,5 +1,6 @@
 package com.type.service;
 
+import com.tpe.CarDTO;
 import com.type.controller.request.CarRequest;
 import com.type.domain.Car;
 import com.type.repository.CarRepository;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 
 
 import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -22,8 +26,15 @@ public class CarService {
 
         Car car =  modelMapper.map(carRequest, Car.class);
         carRepository.save(car);
-
-
+    }
+    public List<CarDTO> getAllCars() {
+     List<Car> carList = carRepository.findAll();
+     List<CarDTO> carDTOList = carList.stream().map(this::mapCarToCarDTO).collect(Collectors.toList());
+    return carDTOList;
+    }
+    private CarDTO mapCarToCarDTO(Car car){
+        CarDTO carDTO = modelMapper.map(car, CarDTO.class);
+        return carDTO;
     }
 
 }
